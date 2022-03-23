7ef1a204a23eb75d4c00d6bd5e8c6c3bb77f63bb
Kai Warren <kaiwarren12@yahoo.com>
2022-03-22 22:54:20 -0700

    Created new portfolio app and started intro component

---
+++ kwreactportfolio/src/App.js
@@ -0,0 +1,11 @@
+import Intro from "./components/intro/intro";
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+};
+
+export default App;
---
+++ kwreactportfolio/src/components/intro/intro.jsx
@@ -0,0 +1,10 @@
+const Intro = () => {
+  return (
+    <div className="i">
+      <div className="i-left"> Left</div>
+      <div className="i-right"> Right </div>
+    </div>
+  );
+};
+
+export default Intro;
