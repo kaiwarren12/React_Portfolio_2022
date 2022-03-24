4258a2326773b4bbdc7c82e9b0aed70d6eddfebc
Kai Warren <kaiwarren12@yahoo.com>
2022-03-24 00:59:36 -0700

    added google fonts. Changed intro css. Properly cloned repo onto desktop and fixed ssh key issues

--- kwreactportfolio/src/components/intro/intro.jsx
+++ kwreactportfolio/src/components/intro/intro.jsx
@@ -1,7 +1,14 @@
+import "./intro.css"
+
 const Intro = () => {
   return (
     <div className="i">
-      <div className="i-left"> Left</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Kai Warren</h1>
+        </div>
+      </div>
       <div className="i-right"> Right </div>
     </div>
   );
