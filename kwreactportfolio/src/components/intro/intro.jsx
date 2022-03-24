df6604770d343c25f6c91d239b35b1522958bf55
Kai Warren <kaiwarren12@yahoo.com>
2022-03-24 16:03:33 -0700

    updated title div and animated it using keyframes

--- kwreactportfolio/src/components/intro/intro.jsx
+++ kwreactportfolio/src/components/intro/intro.jsx
@@ -7,6 +7,13 @@ const Intro = () => {
         <div className="i-left-wrapper">
           <h2 className="i-intro">Hello, My name is</h2>
           <h1 className="i-name">Kai Warren</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item"> Full Stack Web Developer </div>
+              <div className="i-title-item">SASS Account Management </div>
+              <div className="i-title-item">Email Specialist </div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="i-right"> Right </div>
